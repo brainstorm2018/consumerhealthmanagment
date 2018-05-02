02ae314744f8ac08342ecd50cb7a6904aab6c2d6
Mikel <m.kasneci@evolutivo.it>
2018-05-02 15:49:39 +0200

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { QuestionComponent } from './component/question/question.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule
