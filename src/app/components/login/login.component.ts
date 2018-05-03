e5e1227411a1916daeff3735899f1a722ce0bc44
Mikel Kasneci <mikelkasneci@hotmail.com>
2018-05-04 00:35:55 +0200

    Made the login wizard

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  isLoginShow = true;
+  isEnterPhoneNumberShow = false;
+  isEnterCodeShow = false
   constructor() { }
 
   ngOnInit() {
+    this.isLoginShow = true;
+    this.isEnterPhoneNumberShow = false;
+    this.isEnterCodeShow = false
+  }
+
+
+  loginWithGmail() {
+    this.isLoginShow = false;
+    this.isEnterPhoneNumberShow = true;
+  }
+
+  submitPhoneNumber() {
+    this.isEnterPhoneNumberShow = false;
+    this.isEnterCodeShow = true;
+  }
+
+  submitLogin() {
+
+  }
+
+  backEvent() {
+    if (this.isEnterPhoneNumberShow) {
+      this.isLoginShow = true;
+      this.isEnterPhoneNumberShow = false;
+    } else if (this.isEnterCodeShow) {
+      this.isEnterPhoneNumberShow = true;
+      this.isEnterCodeShow = false;
+    }
   }
 
 }
