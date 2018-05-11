5ad7f84260020fada901b0298d096aa410a1ab77
Mikel Kasneci <mikelkasneci@hotmail.com>
2018-05-12 00:45:51 +0200

    create&fix(blood presure) Create timepicker, and added the datePicker

--- src/app/components/blood-presure/blood-presure.component.ts
+++ src/app/components/blood-presure/blood-presure.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blood-presure.component.css']
 })
 export class BloodPresureComponent implements OnInit {
-  model;
+  model:any={
+  };
+  time = {hour: 13, minute: 30};
   constructor() { }
 
   ngOnInit() {
+    this.model.year = new Date().getFullYear();
+    this.model.month = new Date().getMonth()+1;
+    this.model.day = new Date().getDate();
+  }
+
+  testClick() {
+      console.log(this.model);
   }
 
 }
